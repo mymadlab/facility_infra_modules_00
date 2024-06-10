f5297dcaa885ccc4f283428385526f02ccae4ea1
Bryce Klimoski <biggiebk@mymadlab.net>
2024-06-09 20:09:11 -0700

    Enable protected main branch

--- modules/mml_module_builder/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "tfe_modules" {
-  description = "Map of the module with all it's information"
-  value       = module.tfe_module_manager
-}
-
-output "module_repositories" {
-  description = "Map of the github repo with all it's information"
-  value       = module.github_repository_manager
-}
